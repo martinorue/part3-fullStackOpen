94968ca76baca0b05868280c98a887d101c0b98c
martinorue <martin.orue008@gmail.com>
2022-07-11 13:26:50 -0300

    3.5: Phonebook backend step5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         "id": 1,
@@ -55,6 +57,29 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(p => p.id !== id)
 
     response.status(204).end()
+})
+
+const getRandomInt = max => Math.floor(Math.random() * max)
+
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    const person = {
+        id: getRandomInt(Number.MAX_SAFE_INTEGER),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 const PORT = 3001
