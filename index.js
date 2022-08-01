fa1dcac5b2e096ceeb2df79d7edd9150bd3c065a
martinorue <martin.orue008@gmail.com>
2022-08-01 14:57:06 -0300

    3.13: Phonebook database, step1 - for testing

--- index.js
+++ index.js
@@ -2,78 +2,18 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
-const mongoose = require('mongoose')
-
 
 app.use(express.json())
 app.use(express.static('build'))
 app.use(morgan(':url :method :body'));
 app.use(cors())
 
-
-
-const url = `mongodb+srv://martinorue:${password}@cluster0.5qybt.mongodb.net/agendaApp?retryWrites=true&w=majority`
-
-mongoose.connect(url)
-
-const personSchema = new mongoose.Schema({
-    name: String,
-    number: Number,
-})
-
-const Person = mongoose.model('Person', personSchema)
-
-
-mongoose
-    .connect(url)
-    .then(() => {
-        if (process.argv.length === 3) {
-            Person.find({}).then(result => {
-                console.log('phonebook');
-                result.forEach(person => {
-                    console.log(`${person.name} ${person.number}`);
-                })
-                mongoose.connection.close()
-            }).then(() => mongoose.connection.close())
-        } else {
-            const person = new Person({
-                name: name,
-                number: number
-            })
-            return person.save().then(() => {
-                console.log(`added ${person.name} number ${person.number} to phonebook`)
-                return mongoose.connection.close()
-            })
-        }
-    }).catch((err) => console.log(err))
-
-
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
 morgan.token('body', (req) => JSON.stringify(req.body))
 
 app.get('/', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons', morgan('tiny'), (request, response) => {
@@ -83,14 +23,15 @@ app.get('/api/persons', morgan('tiny'), (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id)
 
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
 
 })
 
