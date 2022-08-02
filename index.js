2c126088c08d6058dc7e2eda8fb10f71ad3af8a3
martinorue <martin.orue008@gmail.com>
2022-08-02 13:37:54 -0300

    delete for testing

--- index.js
+++ index.js
@@ -54,10 +54,11 @@ app.get('/info', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', morgan('tiny'), (request, response) => {
